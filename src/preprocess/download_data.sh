c88f6a1a389226f840dabbed683d14e376305e28
Joshua Niemelä <josh@jniemela.dk>
2023-03-02 14:04:58 +0100

    Added push and popd

--- src/preprocess/download_data.sh
+++ src/preprocess/download_data.sh
@@ -2,11 +2,13 @@
 
 TEMPDIR=$(mktemp -d)
 
+pushd $TEMPDIR
 # Download all of the 9 parts including the main zip
 for i in {01..09} "ip"
 do
   wget "https://github.com/several27/FakeNewsCorpus/releases/download/v1.0/news.csv.z$i"
 done
+popd
 
 echo "Done downloading files:\n"
 
