184dc5a19127b8a9fea2997f181f08cd3fcf36b7
Jakup Lutzen <jakup@lutzen.co>
2023-03-07 13:18:52 +0100

    download data now downloads, uncompresses and removes CR

--- src/preprocess/download_data.sh
+++ src/preprocess/download_data.sh
@@ -8,9 +8,16 @@ for i in {01..09} "ip"
 do
   wget "https://github.com/several27/FakeNewsCorpus/releases/download/v1.0/news.csv.z$i"
 done
-popd
 
 echo "Done downloading files:\n"
 
-echo "Extracting archives"
-7z x $TEMPDIR/news.csv.zip -y -o"../../data" && rm -r $TEMPDIR
+echo "Concatinating archives"
+cat news.csv.{z01,z02,z03,z04,z05,z06,z07,z08,z09,zip} > temp.zip
+rm news.csv.z*
+echo "Extracting archive"
+unzip temp.zip
+rm temp.zip
+sed 's/\r/ /g' news_cleaned_2018_02_13.csv > cr_removed.csv
+rm news_cleaned_2018_02_13.csv
+popd
+mv $TEMPDIR\cr_removed.csv .
