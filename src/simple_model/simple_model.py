9212a10450fd72725360ff4a316f7d10eb58522d
Joshua Niemelä <josh@jniemela.dk>
2023-03-26 15:13:02 +0200

    more prints

--- src/simple_model/simple_model.py
+++ src/simple_model/simple_model.py
@@ -28,15 +28,18 @@ except:
         max_features=2048, sublinear_tf=True, preprocessor=pass_fun, tokenizer=pass_fun
     )
 
+    print("fitting TF-IDF")
     X_train = tfidf.fit_transform(X_train)
     print("saving tfidf model")
     pickle.dump(tfidf, open("pickles/tfidf-2048.pkl", "wb"))
 
+    print("fitting SVD")
     svd = TruncatedSVD(n_components=256, random_state=42)
     X_train = svd.fit_transform(X_train)
     print("saving svd model")
     pickle.dump(svd, open("pickles/svd-256.pkl", "wb"))
 
+    print("fitting logistic regressor")
     lr = LogisticRegression(random_state=42)
     lr.fit(X_train, y_train)
 
