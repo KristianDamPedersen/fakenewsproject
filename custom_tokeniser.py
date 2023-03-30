8913c88745892c9081f96a65401d7f35449852fe
Joshua Niemelä <josh@jniemela.dk>
2023-03-30 21:25:43 +0200

    Added documentation and removed old umap file

--- custom_tokeniser.py
+++ custom_tokeniser.py
@@ -4,20 +4,22 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 
 
-# Custom tokenizer with lemmatization
+# Custom tokenizer with lemmatisation.
 def custom_tokenizer(text):
+    # We first lower the string
     lower_text = text.lower()
 
-    # Tokenize with RegexpTokenizer
+    # Tokenise with RegexpTokenizer which is matching on any word separated by some sort of border
     tokenizer = RegexpTokenizer(r"\b[a-z]+\b")
     tokenized_words = tokenizer.tokenize(lower_text)
 
     # Load English stopwords
     stopwords_set = set(stopwords.words("english"))
 
-    # Remove stopwords
+    # Remove stop words
     filtered_words = [word for word in tokenized_words if word not in stopwords_set]
 
+    # Use WordNet to lemmatise the words into their base form
     lemmatizer = WordNetLemmatizer()
     lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
     return lemmatized_words
--- tokenise.py
+++ tokenise.py
@@ -6,6 +6,7 @@ import pandas as pd
 import glob
 
 
+# This function takes a parquet dataset and tokenises it according to the custom tokeniser we've implemented
 def tokenise_parquet(parquet_file):
     print(f"Reading {parquet_file}")
     df = pd.read_parquet(parquet_file)
@@ -14,6 +15,7 @@ def tokenise_parquet(parquet_file):
     print(f"Finished writing {parquet_file}")
 
 
+# Take one command argument which is the parquet dataset and apply the function above to it
 if __name__ == "__main__":
     parquet_dir = sys.argv[1]
     parquet_files = glob.glob(parquet_dir + "/*.parquet")
