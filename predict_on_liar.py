1157e1f0bf42baf3b1e212571f846da2d6fcbfcc
KristianPedersen <kristian.dam.pedersen@gmail.com>
2023-03-30 19:44:47 +0200

    kristians work

--- predict_on_liar.py
+++ predict_on_liar.py
@@ -1,4 +1,6 @@
+
 import pandas as pd
+import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +11,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, roc_curve, roc_auc_score, auc
+from sklearn.metrics import classification_report, roc_curve, roc_auc_score, auc, confusion_matrix
 from tensorflow.keras.models import Sequential, load_model
 
 def main():
@@ -50,7 +52,10 @@ def main():
         }
     ]
 
-# Plot the ROC curve
+    evaluate_pipelines(pipelines, models, X_tokens, y)
+    plot_roc_curve(pipelines)
+
+def plot_roc_curve(pipelines):
     plt.figure()
     plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
     plt.xlim([0.0, 1.0])
@@ -58,10 +63,10 @@ def main():
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('ROC on LIAR dataset')
-    evaluate_pipelines(pipelines, models, X_tokens, y)
+    for pipeline in pipelines:
+        plt.plot(pipeline['fpr'], pipeline['tpr'], color=pipeline['color'], lw=1, label=f'{pipeline["name"]} (AUC = %0.3f)' % pipeline['roc_auc'])
     plt.legend(loc="lower right")
     plt.savefig('report/src/figures/ROC_LIAR.png')
-
 
 
 def load_models():
@@ -110,14 +115,51 @@ def evaluate_pipeline(pipeline, models, X, y):
         y_pred = [b for a,b in y_pred]
         y_pred_binary = model.predict(X)
 
-
     print(f"Results for {pipeline['name']}")
     print(classification_report(y, y_pred_binary))
-
-    # Calculate the ROC curve, AUC score and add the line
+    save_matrix(pipeline['name'], y, y_pred_binary)
+    
+    # Calculate the ROC curve, AUC score and store the values in the pipeline dictionary
     fpr, tpr, _ = roc_curve(y, y_pred)
     roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, color=pipeline['color'], lw=1, label=f'{pipeline["name"]} (AUC = %0.3f)' % roc_auc)
+    pipeline['fpr'] = fpr
+    pipeline['tpr'] = tpr
+    pipeline['roc_auc'] = roc_auc
+
+def save_matrix(name, y_true, y_pred):
+
+    path='report/src/figures/'
+    # Make confusion matrix
+    conf_matrix = confusion_matrix(y_true, y_pred, normalize="true")
+
+    # Create a new figure and axis
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    # Plot confusion matrix
+    im = ax.imshow(conf_matrix, interpolation="nearest", cmap=plt.cm.gray_r)
+
+    # Define labels
+    labels = [['TN', 'FP'], ['FN', 'TP']]
+
+    for i in range(2):
+        for j in range(2):
+            ax.text(
+                j,
+                i,
+                f"{labels[i][j]}: {format(conf_matrix[i, j], '.2f')}",
+                horizontalalignment="center",
+                color="white" if conf_matrix[i, j] > 0.5 else "black",
+            )
+
+    ax.set_xlabel("Predicted label")
+    ax.set_ylabel("True label")
+    ax.axis('off')
+    ax.set_title(name)
+
+    # Save confusion matrix as PNG file
+    fig.savefig(path+name+'_conf_matrix.png', dpi=300, bbox_inches='tight')
+
+
 
 
 if __name__ == '__main__': 
