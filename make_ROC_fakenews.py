1094005d7fd9e47d1ed5f575445eed1bfdacd50c
Jakup Lutzen <jakup@lutzen.co>
2023-03-30 17:21:39 +0200

    added final ROC_curve figure

--- make_ROC_fakenews.py
+++ make_ROC_fakenews.py
@@ -68,7 +68,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('ROC Curves, FakeNews dataset')
+plt.title('ROC on FakeNews dataset')
 plt.legend(loc="lower right")
 plt.savefig('report/src/figures/ROC_curves_fakenews.png')
 
